8e17ec6eaa1767f19557623a3524bbd6a2b344e2
minki <alsrl0023@naver.com>
2024-03-05 11:43:31 +0900

    feat: Searchbar UI 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
+  const [searchWord, setSearchWord] = useState("");
+
+  const inputOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchWord(e.target.value);
+  };
+
+  const buttonOnClickHandler = () => {
+    if (searchWord.trim().length < 1) {
+      alert("검색어를 입력하세요");
+      setSearchWord("");
+    }
+    console.log("e", searchWord);
+  };
+
   return (
-    <div>
-      <div id="search-bar">
-        <Input type="text" placeholder="write subway name" />
-        <Button type="submit">Search!</Button>
+    <div id="home" className="h-5/6">
+      <div id="search-bar" className="flex max-w-[680px] m-auto gap-2">
+        <Input
+          type="text"
+          placeholder="write subway name"
+          onChange={inputOnChangeHandler}
+          value={searchWord}
+        />
+        <Button type="button" onClick={buttonOnClickHandler}>
+          Search
+        </Button>
       </div>
     </div>
   );
