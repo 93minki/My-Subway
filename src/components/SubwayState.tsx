e4cb275ebd706de4d98ee35ca9c7e46d1e92e8a0
minki <alsrl0023@naver.com>
2024-03-07 16:11:33 +0900

    fix: subwayList를 가져오는 곳에서 심각한 오류가 있었음

--- src/components/SubwayState.tsx
+++ src/components/SubwayState.tsx
@@ -1,7 +1,26 @@
 import useSearchResultStore from "@/stores/searchResult";
+import info from "../subway_info.json";
 import type { realTimeArrivalListType } from "../types/ResponseType";
 import LineState from "./LineState";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+
+interface SubwayInfoType {
+  [key: string]: {
+    name: string;
+    color: string;
+    station: {
+      [key: string]: string;
+    };
+  };
+}
+
+const subwayInfo: SubwayInfoType = info;
+/**
+ * 정왕역을 검색했을 때 에러가 발생하는데, 정왕역에는 4호선과 수인분당선이 지나고 있다.
+ * 따라서 const subwayIdListString = searchResult.realtimeArrivalList[0].subwayList; 여기에 당연히 "1004,1075"가 올것
+ * 이라고 생각했는데 첫 번째가 4호선만 있는 열차여서 "1004"가 되었고 당연히 1075를 추가하려고하니까 에러가 발생함
+ * 그렇다면 subwayList중 가장 긴 것을 찾아서 하던가.. 아니면 Set 같은걸로 만들어야 겠는데?
+ */
 
 const SubwayState = () => {
   const searchResult = useSearchResultStore((state) => state.searchResult);
@@ -18,37 +37,37 @@ const SubwayState = () => {
   }
 
   // NOTE 환승 역 ID List
-  const subwayIdListString = searchResult.realtimeArrivalList[0].subwayList;
-  const subwayIdList = subwayIdListString.split(",");
-  const subwayObject = subwayIdList.reduce<{
-    [key: string]: realTimeArrivalListType[];
-  }>((acc, cur) => {
-    acc[cur] = [];
-    return acc;
-  }, {});
+  const subwayObject: { [key: string]: realTimeArrivalListType[] } = {};
 
   searchResult.realtimeArrivalList.forEach((list) => {
-    subwayObject[list.subwayId].push(list);
+    if (Object.keys(subwayObject).includes(list.subwayId)) {
+      subwayObject[list.subwayId].push(list);
+    } else {
+      subwayObject[list.subwayId] = [list];
+    }
   });
 
-  console.log("subwayObject", subwayObject);
-
-  // TODO 여기서 Tabs으로 구분해야 할 듯??
-  // TabList가 될 것 들은 subwayIdList를 사용해서 만들어야 할 것 같음
-
   return (
-    <Tabs defaultValue={subwayIdList[0]}>
-      <TabsList>
-        {subwayIdList.map((list) => (
-          <TabsTrigger value={list}>{list}</TabsTrigger>
+    searchResult && (
+      <Tabs defaultValue={Object.keys(subwayObject)[0]}>
+        <TabsList>
+          {Object.keys(subwayObject).map((list) => {
+            console.log("list", list);
+            console.log("defaultValue", Object.keys(subwayObject)[0]);
+            return (
+              <TabsTrigger key={Math.random()} value={list}>
+                {subwayInfo[list].name}
+              </TabsTrigger>
+            );
+          })}
+        </TabsList>
+        {Object.keys(subwayObject).map((list) => (
+          <TabsContent key={Math.random()} value={list}>
+            <LineState key={Math.random()} lineList={subwayObject[list]} />
+          </TabsContent>
         ))}
-      </TabsList>
-      {subwayIdList.map((list) => (
-        <TabsContent value={list}>
-          <LineState lineList={subwayObject[list]} />
-        </TabsContent>
-      ))}
-    </Tabs>
+      </Tabs>
+    )
   );
 };
 
