61cc8f91a26b62cdeca5cc7f08b11af9afc30e41
Minki Kim <alsrl0023@naver.com>
2024-03-09 19:38:16 +0900

    feat: PWA 설치 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,58 @@
 import SearchBar from "@/components/SearchBar";
 import SubwayState from "@/components/SubwayState";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
+}
+
+let defferedPrompt: BeforeInstallPromptEvent | null = null;
 
 export default function Home() {
+  const [isInstallable, setIsInstallable] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: BeforeInstallPromptEvent) => {
+      e.preventDefault();
+      defferedPrompt = e;
+      setIsInstallable(true);
+    };
+
+    window.addEventListener(
+      "beforeinstallprompt",
+      handleBeforeInstallPrompt as EventListener
+    );
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt as EventListener
+      );
+    };
+  }, []);
+
+  const showPWAInstallPrompt = () => {
+    if (defferedPrompt) {
+      defferedPrompt.prompt();
+      defferedPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("사용자가 설치를 수락함");
+        } else {
+          console.log("사용자가 설치를 거절함");
+        }
+        defferedPrompt = null;
+        setIsInstallable(false);
+      });
+    }
+  };
+
   return (
     <div id="home" className="flex flex-col gap-16 max-w-[418px] w-full">
+      {isInstallable && (
+        <Button onClick={showPWAInstallPrompt}>PWA를 설치하세용</Button>
+      )}
       <SearchBar />
       <SubwayState />
     </div>
