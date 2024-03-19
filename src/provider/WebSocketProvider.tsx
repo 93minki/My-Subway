9ef7dbfc24a531964cf935d9d8c04285fd76e91b
Minki Kim <alsrl0023@naver.com>
2024-03-19 22:29:14 +0900

    feat: WebSocketProvider 재연결 로직 추가

--- src/provider/WebSocketProvider.tsx
+++ src/provider/WebSocketProvider.tsx
@@ -1,14 +1,18 @@
 import useSearchResultStore from "@/stores/searchResult";
-import useTrackSubwayStore from "@/stores/trackSubway";
-import React, { createContext, useContext, useEffect, useRef } from "react";
+import React, {
+  MutableRefObject,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
 
-// WebSocketContext에 대한 TypeScript 타입을 정의합니다.
-interface IWebSocketContext {
-  ws: React.MutableRefObject<WebSocket | null>;
+interface WebSocketContextType {
+  ws: MutableRefObject<WebSocket | null>;
 }
 
-// WebSocketContext를 생성할 때 null이 아닌, 실제 타입으로 초기화합니다.
-const WebSocketContext = createContext<IWebSocketContext | null>(null);
+const WebSocketContext = createContext<WebSocketContextType | null>(null);
 
 export const WebSocketProvider = ({
   children,
@@ -19,38 +23,44 @@ export const WebSocketProvider = ({
   const setSearchResult = useSearchResultStore(
     (state) => state.setSearchResult
   );
-  const setSubwayNumber = useTrackSubwayStore((state) => state.setSubwayNumber);
+
+  const initializeWebSocket = useCallback(() => {
+    console.log("WebSocket 연결 시도");
+    ws.current = new WebSocket(`${import.meta.env.VITE_WS_ENDPOINT}`);
+    ws.current.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      console.log("message?", message);
+      setSearchResult(message);
+    };
+
+    ws.current.onerror = (event) => {
+      console.error("Websocket Error: ", event);
+    };
+
+    ws.current.onclose = (event) => {
+      console.log("WebSocket Connection Closed", event);
+      ws.current = null;
+    };
+  }, [setSearchResult]);
+
+  const handleVisibilityChange = useCallback(() => {
+    if (document.visibilityState === "visible" && !ws.current) {
+      initializeWebSocket();
+    }
+  }, [initializeWebSocket]);
+
   useEffect(() => {
     if (!ws.current) {
-      console.log("연결 시도");
-      ws.current = new WebSocket(`${import.meta.env.VITE_WS_ENDPOINT}`);
-
-      ws.current.onmessage = (event) => {
-        const responseAll = JSON.parse(event.data);
-        console.log("responseAll", responseAll);
-
-        const subwayData = responseAll.subwayData;
-        if (responseAll.stopMessage !== "") {
-          // stopMessage가 존재함
-          setSubwayNumber("");
-        }
-        setSearchResult(subwayData);
-      };
-
-      ws.current.onerror = (event) => {
-        console.error("Websocket Error: ", event);
-      };
-
-      ws.current.onclose = (event) => {
-        console.log("WebSocket Connection Closed", event);
-        ws.current = null;
-      };
+      initializeWebSocket();
     }
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
 
     return () => {
       ws.current?.close();
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
-  }, []);
+  }, [handleVisibilityChange, initializeWebSocket]);
 
   // context 값에는 ws를 포함하는 객체를 전달합니다.
   return (
