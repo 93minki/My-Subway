21a425663c64cdc16a9a7d53c52a99aaa3e9bf07
minki <alsrl0023@naver.com>
2024-04-04 17:50:37 +0900

    feat: history 삭제 기능 추가

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -10,14 +10,19 @@ const SearchBar = () => {
       <div id="search-bar" className="flex max-w-[390px] w-full m-auto gap-2">
         <Input
           type="text"
-          placeholder="write subway name"
+          placeholder="지하철 역이름을 검색해주세요 (역 제외)"
           onChange={(e) => {
             setSearchWord(e.target.value);
           }}
           value={searchWord}
+          onKeyDown={async (e) => {
+            if (e.key === "Enter") {
+              await handleSearch();
+            }
+          }}
         />
         <Button type="button" onClick={handleSearch}>
-          Search
+          검색
         </Button>
       </div>
     </div>
--- src/components/SearchHistory.tsx
+++ src/components/SearchHistory.tsx
@@ -1,4 +1,5 @@
 import useSearchWordStore from "@/stores/searchWord";
+import { VscError } from "react-icons/vsc";
 import { Button } from "./ui/button";
 
 const SearchHistory = () => {
@@ -6,19 +7,35 @@ const SearchHistory = () => {
   const searchWordHistory = useSearchWordStore(
     (state) => state.searchWordHistory
   );
+  const setSearchWordHistory = useSearchWordStore(
+    (state) => state.setSearchWordHistory
+  );
+
+  const deleteSearchWordHistory = (station: string) => {
+    const updateHistory = searchWordHistory.filter(
+      (history) => history !== station
+    );
+    setSearchWordHistory(updateHistory);
+  };
 
   return (
     <div className="flex gap-4">
       {searchWordHistory.map((history) => (
-        <Button
-          key={history}
-          className="bg-gray-500 text-sm"
-          onClick={() => {
-            setSearchWord(history);
-          }}
-        >
-          {history}
-        </Button>
+        <div className="relative group">
+          <Button
+            key={history}
+            className="bg-gray-500 text-sm"
+            onClick={() => {
+              setSearchWord(history);
+            }}
+          >
+            {history}
+          </Button>
+          <VscError
+            className="absolute top-0 right-0 invisible text-white group-hover:visible"
+            onClick={() => deleteSearchWordHistory(history)}
+          />
+        </div>
       ))}
     </div>
   );
