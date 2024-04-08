ba22cee20b01a050bdb0e96a0f00762338980328
minki <alsrl0023@naver.com>
2024-04-08 17:47:27 +0900

    feat: SearchBar 테스트 추가

--- jest.config.ts
+++ jest.config.ts
@@ -9,13 +9,23 @@ const config: JestConfigWithTsJest = {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.ts",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testPathIgnorePatterns: ["/node_modules/"],
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
-
+  globals: {
+    "import.meta": {
+      env: {
+        VITE_API_ENDPOINT: "http://localhost:9090",
+        VITE_WS_ENDPOINT: "ws://localhost:9090",
+        VITE_VAPID_PUBLIC_KEY:
+          "BC7SNZAvIUvYNP2_zb8O0aMSNaAEw5jguX2gRHjuf9hy34ZiRz-p9wmeZDDxrbH1cCYtnjehxI3GLYJ_g9gXwic",
+      },
+    },
+  },
 };
 
 export default config;
--- src/__tests__/SearchBar.test.tsx
+++ src/__tests__/SearchBar.test.tsx
@@ -1,13 +1,58 @@
-import {render} from "@testing-library/react"
-import SearchBar from "../components/SearchBar"
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SearchBar from "../components/SearchBar";
 
-it("검색어를 입력하고 엔터를 누르거나 검색 버튼을 클릭하면 검색을 실행한다.", async () => {
+jest.mock("../stores/searchWord", () => ({
+  __esModule: true,
+  default: jest.fn(() => ({
+    searchWord: "",
+    setSearchWord: jest.fn(),
+    searchWordHistory: [],
+    setSearchWordHistory: jest.fn(),
+  })),
+}));
+jest.mock("../hooks/useSearchByWebSocket", () => ({
+  __esModule: true,
+  default: () => ({
+    sendSearchWord: jest.fn(),
+  }),
+}));
 
-  render(<SearchBar/>)
+beforeAll(() => {
+  window.alert = jest.fn();
 });
 
-it(
-  "검색어의 길이가 1이하일 경우 '두 글자 이상의 검색어를 입력해주세요.' alert이 실행된다."
-);
+describe("SearchBar 컴포넌트 통합 테스트", () => {
+  it("SearchBar 컴포넌트 렌더링 테스트", () => {
+    render(<SearchBar />);
+    expect(
+      screen.getByPlaceholderText("지하철 역이름을 검색해주세요 (역 제외)")
+    ).toBeInTheDocument();
+  });
 
-it("검색을 실행하면, 로컬 스토리지에 검색 결과가 저장된다.");
+  it("두 글자 이상의 검색어를 입력하면 alert이 나오지 않는다.", async () => {
+    render(<SearchBar />);
+    const input = screen.getByPlaceholderText(
+      "지하철 역이름을 검색해주세요 (역 제외)"
+    );
+    await userEvent.type(input, "서울역");
+    const searchButton = screen.getByRole("button", { name: "검색" });
+    await userEvent.click(searchButton);
+
+    expect(window.alert).not.toHaveBeenCalledWith();
+  });
+
+  it("한 글자 이하의 검색어를 입력하면 alert('두 글자 이상의 검색어를 입력해주세요.')을 보여준다", async () => {
+    render(<SearchBar />);
+    const input = screen.getByPlaceholderText(
+      "지하철 역이름을 검색해주세요 (역 제외)"
+    );
+    await userEvent.type(input, "서");
+    const searchButton = screen.getByRole("button", { name: "검색" });
+    await userEvent.click(searchButton);
+
+    expect(window.alert).toHaveBeenCalledWith(
+      "두 글자 이상의 검색어를 입력해주세요."
+    );
+  });
+});
