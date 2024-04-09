4628143e18b6958869cc6b8bbe7f281e4abb0ff2
minki <alsrl0023@naver.com>
2024-04-09 12:14:20 +0900

    feat: SearchBar 검색어 저장 테스트 추가

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,25 @@
 import "@testing-library/jest-dom";
+
+const localStorageMock = (function () {
+  let store = {} as Storage;
+
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+    setItem(key: string, value: string) {
+      store[key] = value;
+    },
+    clear() {
+      store = {} as Storage;
+    },
+    removeItem(key: string) {
+      delete store[key];
+    },
+    getAll() {
+      return store;
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
